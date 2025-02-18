66038a180a8e1af21bb424aefdd51a0bc0acbbcf
AkashMAbrrar <madborakash48@gmail.com>
2025-02-19 02:09:11 +0600

    this commit for baseUrl

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://edu-manage-server-three.vercel.app",
 });
-
-//  baseURL: "https://edu-manage-server-three.vercel.app",
 
 const useAxiosPublic = () => {
   return axiosPublic;
